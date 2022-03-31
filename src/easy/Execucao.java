7ef7be83b746e4d9da4379d27d0befdb1ea9c46a
Gabriela Justo <gabriela.jcassia@gmail.com>
2022-03-31 17:43:31 -0300

    exercicio2-3 refatoração

---
+++ src/easy/Execucao.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Execucao {
+    public static void main(String[] args) {
+        String resultado1 = Exercicio02.exercicio2("gaby");
+        System.out.println(resultado1); 
+
+        String resultado2 = Exercicio02.exercicio2("qualidade");
+        System.out.println(resultado2);
+    }
+    
+}
--- src/easy/Exercicio02.java
+++ src/easy/Exercicio02.java
@@ -3,9 +3,10 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio02{
-    public static void main(String[] args) {
-       String palavraDigitada; 
-       palavraDigitada  = JOptionPane.showInputDialog("Digite uma palavra!");
-       System.out.println("Voce digitou: "+palavraDigitada);
+    public static String exercicio2(String palavraDigitada) {
+    
+       return "Voce digitou: "+palavraDigitada;
     } 
+
+
 }
