184690a362ac76605cf3f596d235c673ad7e101f
Gabriela Justo <gabriela.jcassia@gmail.com>
2022-04-02 11:45:21 -0300

    src/easy/Exercicio05.java

--- src/easy/Exercicio03.java
+++ src/easy/Exercicio03.java
@@ -1,14 +1,12 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio03 {
 
     public static String exercicio3(int numero1, int numero2 ) {
         
       
                 
-        return "O valor do segundo numero é :" + numero1+"O valor do primeiro numero é :" + numero2;
+        return "O valor do segundo numero é :" + numero1+"\n O valor do primeiro numero é :" + numero2;
        
 
     }
